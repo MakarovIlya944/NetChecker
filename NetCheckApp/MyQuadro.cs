3ea62d9916c049fa1e54c6d77473481b5dd37249
Илья <MakarovIlya944@yandex.ru>
2018-11-08 01:47:23 +0700

    MyQuadro init
    Пробую реализовать сначала квадро дерево

---
+++ NetCheckApp/MyQuadro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NetCheckApp
+{
+	struct Vector2D
+	{
+		double X { get; set; }
+		double Y { get; set; }
+	}
+
+	public class MyQuadro
+	{
+		class QuadroTreeLeaf
+		{
+			public QuadroTreeLeaf[] children;
+			public HashSet<int> container = new HashSet<int>();
+			public Vector2D min, max;
+
+			public QuadroTreeLeaf(Vector2D _min, Vector2D _max) { min = _min; max = _max; }
+		}
+
+		QuadroTreeLeaf root;
+		List<Vector2D> HostArray;
+		double minDist = 1E-0;
+		int minElem = 1;
+
+		public void AddElement(int v, QuadroTreeLeaf curLeaf)
+		{
+
+		}
+	}
+}
