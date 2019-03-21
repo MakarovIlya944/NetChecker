be6f8dccea83f243658546ec25f1714af9cfda70
Илья <MakarovIlya944@yandex.ru>
2019-03-21 23:44:28 +0700

    Add FEM checker

---
+++ NetCheckFEM/NetGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCheckerFEM
+{
+    class NetGenerator
+    {
+        double normar;
+
+        int n, maxiter, iter;
+        double eps;
+
+        double[] gglLU, gguLU, diLU;
+	    double[] gglA, gguA, diA;
+	    double[] diD;
+        int[] ig, jg;
+
+	    double[] r, z, x, p, pr, Ax, Ay, x0;
+    }
+}
---
+++ NetCheckFEM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCheckerFEM
+{
+    public class Program
+    {
+        static void Main()
+        {
+            double[] p = new double[4] { 1, 2, 3, 4 };
+            Vector a = new Vector(p);
+            p[0] = 312;
+            p[1] = 312;
+        }
+    }
+}
